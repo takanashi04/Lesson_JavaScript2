17abfb2bb3176d633289ffde3d07c2b9ce0d3fc9
EC2 Default User <ec2-user@ip-172-31-16-172.ap-northeast-1.compute.internal>
2023-04-09 10:03:56 +0000

    ＋を入力すると演算ができなくなる

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+let result = document.getElementById("result");
+
 function calcDisplay(btn) {
   let btnValue = btn.innerHTML;
-  console.log(btnValue);
-  
-  //数字が表示できない
-  let input = document.querySelector('input');
+
+  if (btnValue == "AC") {
+    result.value = 0;
+  } else if (btnValue == "=") {
+    result.value = eval(result.value);
+  } else {
+      if (result.value == 0 ) {
+        result.value = btnValue;
+    } else if (result.value.slice(-1) === "+") {
+      result.value = result.value.slice(0,1);
+    } else {
+      result.value += btnValue;
+    }
+  }
 }
+
+// function operator(btn) {
+//   let btnValue = btn.innerHTML;
+//   if (result.value.slice(-1) === "+" || "-" || "*" || "/" ) {
+//     return;
+//   } else {
+//     result.value += btnValue;
+//   }
+// }
+
+//初期値の０を消して次の文字を入力
+//→if文で０と入力した値を差し替える
+//記号(+,-,*,/,.)が最初に入力された時は表示しない
+//記号を連続で入力しないようにする
