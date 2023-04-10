727f8c2bac3cbfda55d053959539265f102e0b37
EC2 Default User <ec2-user@ip-172-31-16-172.ap-northeast-1.compute.internal>
2023-04-10 04:01:53 +0000

    電卓課題提出

--- main.js
+++ main.js
@@ -7,29 +7,30 @@ function calcDisplay(btn) {
     result.value = 0;
   } else if (btnValue == "=") {
     result.value = eval(result.value);
-  } else {
-      if (result.value == 0 ) {
+  } else if  (result.value == 0 )
+        if (btnValue == "+"
+         || btnValue == "-"
+         || btnValue == "*"
+         || btnValue == "/"
+         || btnValue == ".") {
+          btnValue == "0";
+        } else {
         result.value = btnValue;
-        //2度押すと＋が消える。
-        //足し算ができなくなる
-    } else if (result.value.slice(-1) === "+") {
-      result.value = result.value.slice(0,1);
-    } else {
+   } else if (result.value.slice(-1) == "+"
+          || result.value.slice(-1) == "-"
+          || result.value.slice(-1) == "*"
+          || result.value.slice(-1) == "/"
+          || result.value.slice(-1) == ".") {
+            if (btnValue == "+"
+             || btnValue == "-"
+             || btnValue == "*"
+             || btnValue == "/"
+             || btnValue == ".") {
+              btnValue == "";
+            } else {
+              result.value += btnValue;
+        }
+   } else {
       result.value += btnValue;
-    }
-  }
+   }
 }
-
-// function operator(btn) {
-//   let btnValue = btn.innerHTML;
-//   if (result.value.slice(-1) === "+" || "-" || "*" || "/" ) {
-//     return;
-//   } else {
-//     result.value += btnValue;
-//   }
-// }
-
-//初期値の０を消して次の文字を入力
-//→if文で０と入力した値を差し替える
-//記号(+,-,*,/,.)が最初に入力された時は表示しない
-//記号を連続で入力しないようにする
